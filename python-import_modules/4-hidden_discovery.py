67f55cab29d79b76922e566740ef7ee13b4517c9
Juleslgc <venturajules99@gmail.com>
2025-05-07 11:43:09 +0200

    feat: Add function hidden_discovery

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import hidden_4
+
+
+def main():
+    name = dir(hidden_4)
+    for i in range(len(name)):
+        if name[i][0] != '_':
+            print("{}".format(name[i]))
+
+
+if __name__ == "__main__":
+    main()
