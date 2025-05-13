99f41490e83e0f3307170a252d10d212f4b780f4
Juleslgc <venturajules99@gmail.com>
2025-05-13 15:46:33 +0200

    fix: fix bug delete []

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,5 @@
 def square_matrix_simple(matrix=[]):
     new_m = []
     for i in matrix:
-        new_m.append([list(map(lambda x: x ** 2, i))])
+        new_m.append(list(map(lambda x: x ** 2, i)))
     return new_m
