ca3d1f0183a9be3de4f9b4c5935227010c2863ba
Juleslgc <venturajules99@gmail.com>
2025-05-16 20:59:10 +0200

    fix: change i by _ and modify comments

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -11,10 +11,10 @@ def print_square(size):
     Prints a square with the character #
 
     parameter:
-    size : Number of character at print.
+    size : The size of the square, must be an integer >= 0.
 
     return :
-    The number of #.
+    None.
     """
     if not isinstance(size, int):
         raise TypeError('size must be an integer')
@@ -22,5 +22,5 @@ def print_square(size):
         raise ValueError('size must be >= 0')
     if size is float and size < 0:
         raise TypeError('size must be an integer')
-    for i in range(size):
+    for _ in range(size):
         print('#' * size)
