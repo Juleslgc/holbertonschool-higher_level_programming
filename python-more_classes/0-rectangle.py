96e45f9c8360a1b4f69b5cdb51a3845621f73e1c
Juleslgc <venturajules99@gmail.com>
2025-05-20 14:39:30 +0200

    feat: add all functions and readme

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is the "0-rectangle" modulo.
+It contains the class :
+rectangle
+"""
+
+
+class Rectangle:
+    """
+    empty rectangle
+    """
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+More Classe and Object in Python
