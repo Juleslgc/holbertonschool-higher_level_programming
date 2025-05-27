d0e2c6602b2596c9cbdb20e895743d8e284f01af
Juleslgc <venturajules99@gmail.com>
2025-05-27 12:53:59 +0200

    fix: fixing code in init

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -27,10 +27,10 @@ class Rectangle(BaseGeometry):
     Create rectangle
     """
     def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
     def area(self):
         return self.__width * self.__height
--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -26,7 +26,7 @@ class Rectangle(BaseGeometry):
     Create rectangle
     """
     def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
