7fd6817bcc085296968ad7db781e9d4a680712b5
Juleslgc <venturajules99@gmail.com>
2025-05-27 11:26:22 +0200

    fix: fixing bug in init

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -44,9 +44,9 @@ class Square(Rectangle):
     Create square
     """
     def __init__(self, size):
-        if self.integer_validator("size", size):
-            self.__size = size
+        self.integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
 
     def area(self):
         return self.__size * self.__size
