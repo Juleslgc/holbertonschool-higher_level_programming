cb6f748aae300c4b60c4bc66849709292a314284
Juleslgc <venturajules99@gmail.com>
2025-06-11 15:33:31 +0200

    feat: add a new function

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""
+It contains the function:
+home
+data
+status
+get_users
+add_user
+"""
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+users = []
+
+
+@app.route("/")
+def home():
+    """
+    Create a function home.
+    """
+    return ("Welcome to the Flask API!")
+
+
+@app.route("/data")
+def data():
+    """
+    Create a function that give the data.
+    """
+    return jsonify(users)
+
+
+@app.route("/status")
+def status():
+    """
+    Create a function that give the status.
+    """
+    return ('OK')
+
+
+@app.route("/users/<username>")
+def get_users(username):
+    """
+    Create a function that search a user.
+    """
+    for user in users:
+        if user["username"] == username:
+            return jsonify(user)
+    return jsonify({"error": "Users Not Found"}), 404
+
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    """
+    Create a function that add a user.
+    """
+    data = request.get_json()
+    if "username" not in data or not data["username"]:
+        return jsonify({"error": "Username is required"}), 400
+    users.append(data)
+    return jsonify({"message": "User added!", "user": data}), 201
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
