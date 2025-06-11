f938697a547b38f613b26b2847050e627660657d
Juleslgc <venturajules99@gmail.com>
2025-06-11 15:51:59 +0200

    fix: Change lower in upper

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -19,32 +19,32 @@ class MyHandler(BaseHTTPRequestHandler):
             dataset = {"name": "John", "age": 30,
                        "city": "New York"}
             self.send_response(200)
-            self.send_header('content-type', 'application/json')
+            self.send_header('Content-Type', 'application/json')
             self.end_headers()
             self.wfile.write(json.dumps(dataset).encode())
         elif self.path == "/status":
             status = {"status": "OK"}
             self.send_response(200)
-            self.send_header('content-type', 'application/json')
+            self.send_header('Content-Type', 'application/json')
             self.end_headers()
             self.wfile.write(json.dumps(status).encode())
         elif self.path == "/":
             self.send_response(200)
-            self.send_header('content-type', 'text/plain')
+            self.send_header('Content-Type', 'text/plain')
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
         elif self.path == "/info":
             info = {"version": "1.0",
                     "description": "A simple API built with http.server"}
             self.send_response(200)
-            self.send_header('content-type', 'application/json')
+            self.send_header('Content-Type', 'application/json')
             self.end_headers()
             self.wfile.write(json.dumps(info).encode())
         else:
             message = {"error": "404 Not Found",
                        "message": "Endpoint not found"}
             self.send_response(404)
-            self.send_header('content-type', 'application/json')
+            self.send_header('Content-Type', 'application/json')
             self.end_headers()
             self.wfile.write(json.dumps(message).encode())
 
