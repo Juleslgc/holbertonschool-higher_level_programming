a754f70dc910efa78b6ce7b52d8b8d8affe78cb5
Juleslgc <venturajules99@gmail.com>
2025-06-11 16:36:46 +0200

    fix: change endpoints and requestHandler

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -3,11 +3,12 @@
 It contains the class:
 MyHandler
 """
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import http.server
 import json
+import socketserver
 
 
-class MyHandler(BaseHTTPRequestHandler):
+class MyHandler(http.server.BaseHTTPRequestHandler):
     """
     Create Handler class for http.server
     """
@@ -23,11 +24,10 @@ class MyHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(json.dumps(dataset).encode())
         elif self.path == "/status":
-            status = {"status": "OK"}
             self.send_response(200)
             self.send_header('Content-Type', 'application/json')
             self.end_headers()
-            self.wfile.write(json.dumps(status).encode('utf-8'))
+            self.wfile.write(b"OK")
         elif self.path == "/":
             self.send_response(200)
             self.send_header('Content-Type', 'text/plain')
@@ -41,15 +41,15 @@ class MyHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(json.dumps(info).encode('utf-8'))
         else:
-            message = {"error": "404 Not Found",
-                       "message": "Endpoint not found"}
             self.send_response(404)
             self.send_header('Content-Type', 'application/json')
             self.end_headers()
-            self.wfile.write(json.dumps(message).encode('utf-8'))
+            self.wfile.write(b"Endpoint not found")
 
 
 if __name__ == "__main__":
-    server = HTTPServer(("", 8000), MyHandler)
-    print("Server running on http://localhost:8000")
-    server.serve_forever()
+    PORT = 8000
+    Handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
