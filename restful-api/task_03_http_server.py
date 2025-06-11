eaf7c33515817cab7dde850d2592de1c69e8d603
Juleslgc <venturajules99@gmail.com>
2025-06-11 16:46:28 +0200

    fix: delete __name__ = "__main__"

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -47,9 +47,7 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             self.wfile.write(b"Endpoint not found")
 
 
-if __name__ == "__main__":
-    PORT = 8000
-    Handler = http.server.SimpleHTTPRequestHandler
-    with socketserver.TCPServer(("", PORT), Handler) as httpd:
-        print("serving at port", PORT)
-        httpd.serve_forever()
+PORT = 8000
+with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
