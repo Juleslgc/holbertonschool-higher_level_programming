c3ce4093d051cd2b9b498cf807f7997eb32e98d5
Juleslgc <venturajules99@gmail.com>
2025-06-27 11:22:50 +0200

    fix: modified for pycode

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -2,12 +2,16 @@
 """
 Defines a State class and creates the states table in the database.
 """
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
+    """
+    Create class State
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
