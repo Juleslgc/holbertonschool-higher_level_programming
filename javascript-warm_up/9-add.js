54086082177eb997975d3f1699199e982e75ea07
Juleslgc <venturajules99@gmail.com>
2025-07-11 21:22:38 +0200

    fix: simplified of code

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
 
-const first_argv = parseInt(process.argv[2]);
-const second_argv = parseInt(process.argv[3]);
-
 function add (a, b) {
   return a + b;
 }
 
-if (isNaN(first_argv) || isNaN(second_argv)) {
-  console.log('NaN');
-} else {
-  console.log(add(first_argv, second_argv));
-}
+const first_argv = parseInt(process.argv[2]);
+const second_argv = parseInt(process.argv[3]);
+
+console.log(add(first_argv, second_argv));
