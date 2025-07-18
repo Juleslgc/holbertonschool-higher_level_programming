fc3ec4f362a9a064b29997bedce5e80ca570790c
Juleslgc <venturajules99@gmail.com>
2025-07-18 11:47:56 +0200

    feat: extending dynamic data display to include SQLite in Flask

---
+++ python-server_side_rendering/task_04_db.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python3
+"""
+This the route of app.
+"""
+from flask import Flask, render_template, request
+import json, csv, sqlite3
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/items')
+def items():
+    with open('items.json', 'r') as file:
+        items = json.load(file).get('items')
+        return render_template('items.html', items=items)
+
+
+@app.route('/products')
+def products():
+    source = request.args.get('source')
+    product = request.args.get('id')
+
+    if source not in ['json', 'csv', 'sql']:
+        return render_template('product_display.html', error='Wrong source')
+    
+    if source == 'json':
+        try:
+            with open('products.json', 'r') as file:
+                products = json.load(file)
+        except FileNotFoundError:
+            return render_template('product_display.html', error='Not file found')
+    
+    elif source == 'csv':
+        try:
+            with open('products.csv', 'r', newline='') as f:
+                read = csv.DictReader(f)
+                products = list(read)
+        except FileNotFoundError:
+            return render_template('product_display.html', error='Not file found')
+
+    elif source == 'sql':
+        try:
+            conn = sqlite3.connect('products.db')
+            conn.row_factory = sqlite3.Row
+            cursor = conn.cursor()
+            if product:
+                cursor.execute('''
+                    SELECT * FROM Products WHERE id = ?;
+                            ''', (product,))
+                result = cursor.fetchone()
+                if result:
+                    return render_template('product_display.html', products=[result])
+                else:
+                    return render_template('product_display.html', error='Product not found')
+            else:
+                cursor.execute('''
+                    SELECT * FROM Products;
+                            ''')
+                result = cursor.fetchall()
+                return render_template('product_display.html', products=result)
+        except Exception:
+            return render_template('product_display.html', error='Not database found')
+        finally:
+            conn.close()
+    
+    else:
+        return render_template('product_display.html', error='Wrong source')
+    
+    if product:
+        products = [p for p in products if str(p.get('id')) == product]
+        if not products:
+            return render_template('product_display.html', error='Product not found')
+    
+    return render_template('product_display.html', products=products)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
+
