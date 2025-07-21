d107d8ea947a46f8217fb0bab59c02d438c4e06a
Juleslgc <venturajules99@gmail.com>
2025-07-21 10:42:35 +0200

    feat: added .py file that return a copy of a list

---
+++ python-everything_is_object/19-copy_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+It contains the function :
+copy_list
+"""
+
+
+def copy_list(a_list):
+    """
+    Function that return a copy of a list.
+    """
+    copy_list = a_list.copy()
+    return copy_list
