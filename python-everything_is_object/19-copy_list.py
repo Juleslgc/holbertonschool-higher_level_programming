86af6195914df514afe54623303009a2604439dd
Juleslgc <venturajules99@gmail.com>
2025-07-21 12:45:04 +0200

    fix: fixing add just return

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-"""
-It contains the function :
-copy_list
-"""
-
-
 def copy_list(a_list):
-    """
-    Function that return a copy of a list.
-    """
-    copy_list = a_list.copy()
-    return copy_list
+    return a_list[:]
